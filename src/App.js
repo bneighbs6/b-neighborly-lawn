d82fa479ddecedba469386b95cbf516cd1ec5951
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-10-23 17:20:59 -0600

    Import bootstrap css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import NavigationBar from './Components/NavigationBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header bg-success text-center">
         <h1>Neighbors Lawn Service</h1>
       </header>
-      <NavigationBar /> 
+
+      <div id="nav-bar">
+        <NavigationBar />
+      </div>
     </div>
   );
 }
