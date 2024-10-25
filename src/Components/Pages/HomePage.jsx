846fd03028d6587828647a5bcb81a58b26ab6fa7
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-10-25 12:23:25 -0600

    Created another card. Need to fix styling issues

--- src/Components/Pages/HomePage.jsx
+++ src/Components/Pages/HomePage.jsx
@@ -1,35 +1,53 @@
 import React from "react";
-import { Button, Card, Container } from "react-bootstrap";
+import { Button, Card, Container, Row, Col } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function HomePage() {
-  // TODO: 10/25/24 Make handleClick function take you to a form to fill out a request
   function handleClick() {
     console.log("button clicked");
   }
 
-  // Create a home page that explains our services offered.
   return (
     <>
       <div className="home-header">
         <h1>Ready to hire a professional to handle your lawn?</h1>
       </div>
       <Container>
-        <div className="home-page-card">
-          <Card style={{ width: "33%" } }>
-            <Card.Title style={{textAlign: 'center'}}>Winter Is Coming</Card.Title>
-            <h4 style={{textAlign: 'center'}}>Don't feel like shoveling snow all winter?</h4>
-            <p>
-              <Button
-                style={{ backgroundColor: "#003f85" }}
-                onClick={handleClick}
-              >
-                Click Here
-              </Button>
-              to receive a quote for simple snow shoveling done professionally.
-            </p>
-          </Card>
-        </div>
+        <Row>
+          <Col md={6}> {/* Each card will take up half the width on medium and larger screens */}
+            <Card className="home-page-card">
+              <Card.Body>
+                <Card.Title>Winter Is Coming</Card.Title>
+                <h4>Don't feel like shoveling snow all winter?</h4>
+                <p>
+                  <Button
+                    style={{ backgroundColor: "#003f85" }}
+                    onClick={handleClick}
+                  >
+                    Click Here
+                  </Button>
+                  to receive a quote for simple snow shoveling done professionally.
+                </p>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={6}> {/* Each card will take up half the width on medium and larger screens */}
+            <Card className="home-page-card">
+              <Card.Body>
+                <Card.Title>We want to mow your lawn</Card.Title>
+                <p>
+                  <Button
+                    style={{ backgroundColor: "#003f85" }}
+                    onClick={handleClick}
+                  >
+                    Click Here
+                  </Button>
+                  to receive a quote for lawn mowing done professionally.
+                </p>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
       </Container>
     </>
   );
