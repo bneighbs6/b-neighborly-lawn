09a6b5d41385aeca7df03fded229b9165941c0c8
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-10-25 12:04:06 -0600

    Created a card for messages to be displayed

--- src/Components/Pages/HomePage.jsx
+++ src/Components/Pages/HomePage.jsx
@@ -1,16 +1,25 @@
 import React from "react";
-import { Button , Card} from "react-bootstrap";
+import { Button, Card, Container } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function HomePage() {
-    function handleClick() {
-        console.log("button clicked")
-    }
-    return (
-      <>
-        <div className="home-page-message justify-">
-        <h2>Winter Is Coming</h2>
-            <h3>Don't feel like shoveling snow all winter?</h3>
-            <h5>
+  // TODO: 10/25/24 Make handleClick function take you to a form to fill out a request
+  function handleClick() {
+    console.log("button clicked");
+  }
+
+  // Create a home page that explains our services offered.
+  return (
+    <>
+      <div className="home-header">
+        <h1>Ready to hire a professional to handle your lawn?</h1>
+      </div>
+      <Container>
+        <div className="home-page-card">
+          <Card style={{ width: "33%" } }>
+            <Card.Title style={{textAlign: 'center'}}>Winter Is Coming</Card.Title>
+            <h4 style={{textAlign: 'center'}}>Don't feel like shoveling snow all winter?</h4>
+            <p>
               <Button
                 style={{ backgroundColor: "#003f85" }}
                 onClick={handleClick}
@@ -18,11 +27,12 @@ function HomePage() {
                 Click Here
               </Button>
               to receive a quote for simple snow shoveling done professionally.
-            </h5>
+            </p>
+          </Card>
         </div>
-        <Card></Card>
-      </>
-    );
+      </Container>
+    </>
+  );
 }
 
 export default HomePage; 
