3d403cfa3b61ddc0a8718add36a7ca034330d85c
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-11-01 14:40:31 -0600

    Update the Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,25 +8,16 @@ function Footer() {
           <div className="col-sm-12 col-md-4 text-center">
             <h3>Contact Info</h3>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Phone: (702)622-8998
+            </p>
+            <p>
+            Email: b.neighborlylawnservices@gmail.com
             </p>
           </div>
           <div className="col-sm-12 col-md-4 text-center">
             <h3>FAQ</h3>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Why a professional? 
             </p>
           </div>
           <div className="col-sm-12 col-md-4 text-center">
