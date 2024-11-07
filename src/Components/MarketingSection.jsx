82c8f7c55eab834a70b098817b4d9ff79fcb2ab6
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-11-07 06:43:17 -0700

    Created MarketingSection but didn't apply it

--- src/Components/MarketingSection.jsx
+++ src/Components/MarketingSection.jsx
@@ -4,7 +4,7 @@ import WinterMarketing from "../Winter Marketing copy.png";
 function MarketingSection() {
     return (
         <>
-        <h2 className="text-center">Winter Special</h2>
+        <h1 className="text-center">Winter Special</h1>
           <div className="marketing-section">
             <img src={WinterMarketing} alt="Snow Shovel Flyer" />
         </div>      
