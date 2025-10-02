78e8f726f245fe32d98e5a139cc7aee5bf20f0ec
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-10-02 10:50:24 -0600

    Update dropdwon menu

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -1,36 +1,48 @@
 import Container from "react-bootstrap/Container";
-import { Nav, Navbar } from "react-bootstrap";
+import { Nav, Navbar, Dropdown } from "react-bootstrap";
 import { Link } from "react-scroll"; // Import Link from react-scroll
 import { useMediaQuery } from "react-responsive";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function NavigationBar() {
   // Tells the Navigation bar if the width of the screen is greater than 680 px
-  const isMobile = useMediaQuery({ query: '(max-width: 680px)' });
+  const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
   console.log ("Mobile device is " + isMobile);
 
   return (
-    <Navbar expand="md" variant="light">
-    <Container>
-      {isMobile ? (
-<Nav>Mobile Device Active</Nav>
-      ) : (
-        <Nav className="mx-auto">
-        <Nav.Link href="/">
-          <button className="uniform-btn-nav">Home</button>
-        </Nav.Link>
-        <Nav.Link href="/services">
-          <button className="uniform-btn-nav">Services</button>
-        </Nav.Link>
-        <Nav.Link href="/request-form">
-          <button className="uniform-btn-nav">Request</button>
-        </Nav.Link>
-        <Nav.Link href="/about">
-          <button className="uniform-btn-nav">About</button>
-        </Nav.Link>
-      </Nav>
-      )}
-    </Container>
-  </Navbar>
+    <Navbar expand="md" variant="none">
+      <Container>
+        {isMobile ? (
+          <Dropdown>
+            <Dropdown.Toggle className="mobile-dropdown-nav">
+              <FontAwesomeIcon icon={faBars} />
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="/">Home</Dropdown.Item>
+              <Dropdown.Item href="/services">Services</Dropdown.Item>
+              <Dropdown.Item href="/request-form">Request</Dropdown.Item>
+              <Dropdown.Item href="/about">About</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        ) : (
+          <Nav className="mx-auto">
+            <Nav.Link href="/">
+              <button className="uniform-btn-nav">Home</button>
+            </Nav.Link>
+            <Nav.Link href="/services">
+              <button className="uniform-btn-nav">Services</button>
+            </Nav.Link>
+            <Nav.Link href="/request-form">
+              <button className="uniform-btn-nav">Request</button>
+            </Nav.Link>
+            <Nav.Link href="/about">
+              <button className="uniform-btn-nav">About</button>
+            </Nav.Link>
+          </Nav>
+        )}
+      </Container>
+    </Navbar>
   );
 }
 
